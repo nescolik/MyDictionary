f0c0ace2edee245487520323256dd89d397af910
Neslihan Özçelik <ozcelik.neslihan@gmail.com>
2021-01-26 22:20:46 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogranciler = new MyDictionary<int, string>();
+
+           
+        }
+    }
+}
